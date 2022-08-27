e66bed6ebcf45abe23d529394ec3e2c857313f05
Jeha <whrydus0714@gmail.com>
2022-08-27 14:01:09 +0900

    1.2 version ( 220827 )
    내가 원했던 롯데호텔알바 뷔페, 연회 알바만을 알람으로 알려주게끔 추가 구현

--- Main.py
+++ Main.py
@@ -1,8 +1,5 @@
 # 데이터를 위한 라이브러리
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
 import time
 
 
@@ -19,7 +16,7 @@ Different_DataFrame = pd.DataFrame()
 new_Hotels_DataFrame = pd.DataFrame()
 
 
-''' < 이 아래로 매 1분마다 실행 > '''
+''' < 이 아래로 반복 실행 > '''
 while(True) : 
 
 
@@ -35,30 +32,32 @@ while(True) :
     Different_DataFrame =  Compare_2_dataframe.Compare_DataFrame( 
                             new_Hotels_DataFrame, saved_Hotels_DataFrame)
 
+    ''' 찾고싶은 호텔알바의 조건 '''
+    # < 현재 조건 >은 근무지에 롯데 & 잠실이라는 단어가 포함되어 있는지
+    # 모집내용이 뷔페가 포함되거나 연회가 포함되어 있는지
+    hotel_Option = ( Different_DataFrame['근무지'].str.contains('롯데') & 
+                 Different_DataFrame['근무지'].str.contains('잠실') )
+    buffet_Option = (  Different_DataFrame['모집내용'].str.contains('뷔페') )
+    banquet_Option = ( Different_DataFrame['모집내용'].str.contains('연회') ) 
 
-    # 만약 Different_DataFrame 내에 정보가 하나라도 존재한다면
-    if ( len( Different_DataFrame ) > 0 )  :
+    # 호텔알바 조건을 포함한 데이터 프레임을 정의
+    Target_Alba_DataFrame = Different_DataFrame[ hotel_Option & ( 
+                                    buffet_Option | banquet_Option) ]
+
+
+    ''' 만약 Target_Alba_DataFrame 내에 정보가 하나라도 존재한다면 '''
+    if ( len( Target_Alba_DataFrame ) > 0 )  :
         
         # 기존 저장되있던 데이터프레임을 새롭게 들고온 데이터프레임으로 바꿔준다.
         saved_Hotels_DataFrame = new_Hotels_DataFrame
 
-        # Diffrent_DataFrame을 날짜와 근무지로 그룹화 시켜준다.
-        # Diff_Data_Group = Different_DataFrame.groupby( ['날짜', '근무지'] ).groups 
-        
-
-        # 목표인 '롯데호텔월드[잠실].'이 Different_DataFrame에 있는지 확인
-        # 확인 후 하나라도 값이 있다면 해당 근무지에 대한 정보가
-        # 추가로 등록되었음을 kakaotalk으로 알림.
-        if ( len( Different_DataFrame[ Different_DataFrame['근무지']
-                                    == '롯데호텔월드[잠실].']) ) :
-            # 텔레그램으로 알려주기!
-            # 날짜와 근무지에 대한 정보를 넘겨주고 앱에서 메시지를 보낸다.
-            for x in Different_DataFrame[ Different_DataFrame['근무지']
-                                    == '롯데호텔월드[잠실].'].values :
-                texts = str(x)
-                Link_with_Telegram.send_message( texts )
-                time.sleep(1)
+        # 텔레그램으로 알려주기!
+        # 날짜와 근무지에 대한 정보를 넘겨주고 앱에서 메시지를 보낸다.
+        for x in Target_Alba_DataFrame.values :
+            texts = str(x)
+            Link_with_Telegram.send_message( texts )
+            time.sleep(1)
                 
     
     # 잠시 쉬어준 후 다시 반복
-    time.sleep(50) # 60이 아닌 50인 이유는 soup을 통해 데이터를 가져오는 일에 시간이 걸리기 때문.
+    time.sleep(30)
